a194f96161c5a7300c128b467bdc33eb73483833
Jakub Zaprzalka <jakub.zaprzalka@nask.pl>
2024-10-10 14:26:12 +0200

    Update run

--- rasa/core/run.py
+++ rasa/core/run.py
@@ -31,8 +31,12 @@ from rasa.core.channels.channel import InputChannel
 from rasa.core.utils import AvailableEndpoints
 import rasa.shared.utils.io
 from sanic import Sanic
+from sanic.worker.loader import AppLoader
+from sanic.worker.manager import WorkerManager
 from asyncio import AbstractEventLoop
 
+
+WorkerManager.THRESHOLD = 5 * 60 * 10  # 5 minutes for workerrs to come online
 
 logger = logging.getLogger()  # get the root logger
 
@@ -184,6 +188,78 @@ def configure_app(
     return app
 
 
+def prepare_application(
+    protocol: Text,
+    number_of_workers: int,
+    model_path: Optional[Text] = None,
+    channel: Optional[Text] = None,
+    interface: Optional[Text] = constants.DEFAULT_SERVER_INTERFACE,
+    port: int = constants.DEFAULT_SERVER_PORT,
+    credentials: Optional[Text] = None,
+    cors: Optional[Union[Text, List[Text]]] = None,
+    auth_token: Optional[Text] = None,
+    enable_api: bool = True,
+    response_timeout: int = constants.DEFAULT_RESPONSE_TIMEOUT,
+    jwt_secret: Optional[Text] = None,
+    jwt_private_key: Optional[Text] = None,
+    jwt_method: Optional[Text] = None,
+    endpoints: Optional[AvailableEndpoints] = None,
+    remote_storage: Optional[Text] = None,
+    log_file: Optional[Text] = None,
+    conversation_id: Optional[Text] = uuid.uuid4().hex,
+    use_syslog: Optional[bool] = False,
+    syslog_address: Optional[Text] = None,
+    syslog_port: Optional[int] = None,
+    syslog_protocol: Optional[Text] = None,
+    request_timeout: Optional[int] = None,
+    server_listeners: Optional[List[Tuple[Callable, Text]]] = None,
+) -> Sanic:
+    """Run the API entrypoint."""
+    if not channel and not credentials:
+        channel = "cmdline"
+
+    input_channels = create_http_input_channels(channel, credentials)
+    
+    app = configure_app(
+        input_channels,
+        cors,
+        auth_token,
+        enable_api,
+        response_timeout,
+        jwt_secret,
+        jwt_private_key,
+        jwt_method,
+        port=port,
+        endpoints=endpoints,
+        log_file=log_file,
+        conversation_id=conversation_id,
+        use_syslog=use_syslog,
+        syslog_address=syslog_address,
+        syslog_port=syslog_port,
+        syslog_protocol=syslog_protocol,
+        request_timeout=request_timeout,
+        server_listeners=server_listeners,
+    )
+
+    logger.info(f"Starting Rasa server on {protocol}://{interface}:{port}")
+
+    app.register_listener(
+        partial(load_agent_on_start, model_path, endpoints, remote_storage),
+        "before_server_start",
+    )
+
+    app.register_listener(close_resources, "after_server_stop")
+
+    telemetry.track_server_start(
+        input_channels, endpoints, model_path, number_of_workers, enable_api
+    )
+
+    rasa.utils.common.update_sanic_log_level(
+        log_file, use_syslog, syslog_address, syslog_port, syslog_protocol
+    )
+    
+    return app
+
 def serve_application(
     model_path: Optional[Text] = None,
     channel: Optional[Text] = None,
@@ -212,66 +288,54 @@ def serve_application(
     request_timeout: Optional[int] = None,
     server_listeners: Optional[List[Tuple[Callable, Text]]] = None,
 ) -> None:
-    """Run the API entrypoint."""
-    if not channel and not credentials:
-        channel = "cmdline"
-
-    input_channels = create_http_input_channels(channel, credentials)
-
-    app = configure_app(
-        input_channels,
-        cors,
-        auth_token,
-        enable_api,
-        response_timeout,
-        jwt_secret,
-        jwt_private_key,
-        jwt_method,
-        port=port,
-        endpoints=endpoints,
-        log_file=log_file,
-        conversation_id=conversation_id,
-        use_syslog=use_syslog,
-        syslog_address=syslog_address,
-        syslog_port=syslog_port,
-        syslog_protocol=syslog_protocol,
-        request_timeout=request_timeout,
-        server_listeners=server_listeners,
-    )
-
     ssl_context = server.create_ssl_context(
         ssl_certificate, ssl_keyfile, ssl_ca_file, ssl_password
     )
     protocol = "https" if ssl_context else "http"
-
-    logger.info(f"Starting Rasa server on {protocol}://{interface}:{port}")
-
-    app.register_listener(
-        partial(load_agent_on_start, model_path, endpoints, remote_storage),
-        "before_server_start",
-    )
-
-    app.register_listener(close_resources, "after_server_stop")
-
+    
     number_of_workers = rasa.core.utils.number_of_sanic_workers(
         endpoints.lock_store if endpoints else None
     )
-
-    telemetry.track_server_start(
-        input_channels, endpoints, model_path, number_of_workers, enable_api
+    
+    loader = AppLoader(
+        factory=partial(
+            prepare_application,
+            protocol,
+            number_of_workers,
+            model_path,
+            channel,
+            interface,
+            port,
+            credentials,
+            cors,
+            auth_token,
+            enable_api,
+            response_timeout,
+            jwt_secret,
+            jwt_private_key,
+            jwt_method,
+            endpoints,
+            remote_storage,
+            log_file,
+            conversation_id,
+            use_syslog,
+            syslog_address,
+            syslog_port,
+            syslog_protocol,
+            request_timeout,
+            server_listeners,
+        )
     )
-
-    rasa.utils.common.update_sanic_log_level(
-        log_file, use_syslog, syslog_address, syslog_port, syslog_protocol
-    )
-
-    app.run(
+    app = loader.load()
+    
+    app.prepare(
         host=interface,
         port=port,
         ssl=ssl_context,
         backlog=int(os.environ.get(ENV_SANIC_BACKLOG, "100")),
         workers=number_of_workers,
     )
+    Sanic.serve(primary=app, app_loader=loader)
 
 
 # noinspection PyUnusedLocal
